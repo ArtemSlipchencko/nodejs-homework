b5178fd8fee652d650eacc0d054706be35ea5d9f
ArtemSlipchencko <68220186+ArtemSlipchencko@users.noreply.github.com>
2021-02-11 17:37:33 +0200

    Login

--- user/user.controller.js
+++ user/user.controller.js
@@ -2,8 +2,19 @@ const {Types: {ObjectId}} = require('mongoose');
 const User = require('./User');
 const Joi = require('joi');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 async function registerValidation(req, res, next) {
+    const {
+        body: {email}
+    } = req;
+
+    const user = await User.findOne({email});
+
+    if(!user) {
+        return res.status(409).send('Eail in use')
+    };
+
     const validationRules = Joi.object({
         email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).required(),
         password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
@@ -34,7 +45,57 @@ async function userCreate(req, res) {
     };
 };
 
+async function loginValidation(req, res, next) {
+    const {
+        body: {email}
+    } = req;
+
+    const user = await User.findOne({email});
+
+    if(!user) {
+        return res.status(401).send('Email or password is wrong')
+    };
+
+    const validationRules = Joi.object({
+        email: Joi.string().required(),
+        password: Joi.string().required()
+    });
+
+    const validationResult = validationRules.validate(req.body);
+
+    if(validationResult.error) {
+        return res.status(400).json(validationResult.error)
+    };
+
+    next();
+};
+
+async function userLogin(req, res) {
+    const {
+        body: {email, password}
+    } = req;
+
+    const user = await User.findOne({email});
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if(!isPasswordValid) {
+        return res.status(401).send('Email or password is wrong')
+    };
+
+    const token = jwt.sign(
+        {
+            userId: user._id,
+        },
+        process.env.JWT_SECRET
+    );
+
+    return res.json({token});
+};
+
 module.exports = {
     registerValidation,
-    userCreate
-}
+    userCreate,
+    loginValidation,
+    userLogin
+};
--- user/user.routes.js
+++ user/user.routes.js
@@ -4,5 +4,6 @@ const UserController = require('./user.controller');
 const router = Router();
 
 router.post('/register', UserController.registerValidation, UserController.userCreate);
+router.post('/login', UserController.loginValidation, UserController.userLogin);
 
 module.exports = router;
